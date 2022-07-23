5fd8566d3435d17e9b879d4bbb2d4884eab5c63e
youngSeok0413 <youngSeok0413@gmail.com>
2022-07-24 00:21:40 +0900

    9

--- OpenGL Tutorial/OpenGL Tutorial/main.cpp
+++ OpenGL Tutorial/OpenGL Tutorial/main.cpp
@@ -114,8 +114,14 @@ int main() {
 
 	float positions[] = {
 		-0.5f, -0.5f,
-		0.0f, 0.5f,
-		0.5f, -0.5f
+		0.5f, -0.5f,
+		0.5f,  0.5f,
+		-0.5f, 0.5f
+	};
+
+	unsigned int indices[] = {
+		0, 1, 2,
+		2, 3, 0
 	};
 
 	unsigned int vao;
@@ -126,11 +132,17 @@ int main() {
 	unsigned int buffer;
 	glGenBuffers(1, &buffer);
 	glBindBuffer(GL_ARRAY_BUFFER, buffer);
-	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(positions) * sizeof(float), positions, GL_STATIC_DRAW);
 
 	//vertex layout
 	glEnableVertexArrayAttrib(vao ,0);
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2*sizeof(float), 0);
+
+	//index buffer
+	unsigned int ibo;
+	glGenBuffers(1, &ibo);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices) * sizeof(unsigned int), indices, GL_STATIC_DRAW);
 
 	//shader
 	ShaderSource source = ParseShaderSource("someShader.shader");
@@ -142,10 +154,11 @@ int main() {
 	std::cout << "Fragment" << std::endl;
 	std::cout << source.Fragment << std::endl;
 
+
 	while (!glfwWindowShouldClose(window)) {
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 
 		glfwSwapBuffers(window);
 
